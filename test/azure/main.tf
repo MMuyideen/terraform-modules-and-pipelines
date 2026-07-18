bcb328e8735d31c0b8dfaa9eea3fda79c4d61408
Morex <dejimorenigbade@gmail.com>
2026-07-19 00:59:22 +0100

    Update resource_group_name references to use module output in main.tf

--- test/azure/main.tf
+++ test/azure/main.tf
@@ -1,6 +1,6 @@
 module "resource_group" {
   source              = "../../modules/azure/resource-group"
-  resource_group_name = var.resource_group_name
+  resource_group_name = module.resource_group.name
   location            = var.location
 }
 
@@ -9,7 +9,7 @@ module "vnet" {
   vnet_name           = var.vnet_name
   address_space       = var.address_space
   location            = var.location
-  resource_group_name = var.resource_group_name
+  resource_group_name = module.resource_group.name
   subnets             = var.subnets
 }
 
@@ -17,7 +17,7 @@ module "nsg" {
   source              = "../../modules/azure/nsg"
   nsg_name            = var.nsg_name
   location            = var.location
-  resource_group_name = var.resource_group_name
+  resource_group_name = module.resource_group.name
   security_rule       = var.security_rule
   tags                = var.tags
 }
@@ -25,7 +25,7 @@ module "nsg" {
 module "storage_account" {
   source                   = "../../modules/azure/storage-account"
   storage_account_name     = var.storage_account_name
-  resource_group_name      = var.resource_group_name
+  resource_group_name      = module.resource_group.name
   location                 = var.location
   account_tier             = var.account_tier
   account_replication_type = var.account_replication_type
@@ -35,7 +35,7 @@ module "nic" {
   source                 = "../../modules/azure/nic"
   nic_name               = var.nic_name
   location               = var.location
-  resource_group_name    = var.resource_group_name
+  resource_group_name    = module.resource_group.name
   subnet_id              = module.vnet.subnet_ids[0]
   nsg_id                 = module.nsg.nsg_id
   create_nsg_association = true
@@ -45,7 +45,7 @@ module "public_ip" {
   source              = "../../modules/azure/public-ip"
   pip_name            = var.pip_name
   location            = var.location
-  resource_group_name = var.resource_group_name
+  resource_group_name = module.resource_group.name
   allocation_method   = var.allocation_method
   domain_name_label   = var.domain_name_label
 }
@@ -53,7 +53,7 @@ module "public_ip" {
 module "dns_zone" {
   source                = "../../modules/azure/dns-zone"
   private_dns_zone_name = var.private_dns_zone_name
-  resource_group_name   = var.resource_group_name
+  resource_group_name   = module.resource_group.name
   virtual_network_id    = module.vnet.id
   create_vnet_link      = true
   dns_records           = var.dns_records
