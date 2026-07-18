6e805242da4b97992a38a8cbedc79713faa20fa3
Morex <dejimorenigbade@gmail.com>
2026-07-19 00:54:33 +0100

    Fix container name in Azure backend configuration

--- test/azure/backend.tf
+++ test/azure/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "terraform-backend-rg"
     storage_account_name = "deenterraformstate"
-    container_name       = "tfstate/test"
-    key                  = "azure.tfstate"
+    container_name       = "tfstate"
+    key                  = "test-azure.tfstate"
   }
 }
