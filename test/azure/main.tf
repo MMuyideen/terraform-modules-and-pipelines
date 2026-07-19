e3587d64d64de253a2671ee7d3d590c67602e8da
Morex <dejimorenigbade@gmail.com>
2026-07-19 01:01:04 +0100

    Add outputs for Azure resource group in outputs.tf and update main.tf to reference variable for resource group name

--- test/azure/main.tf
+++ test/azure/main.tf
@@ -1,6 +1,6 @@
 module "resource_group" {
   source              = "../../modules/azure/resource-group"
-  resource_group_name = module.resource_group.name
+  resource_group_name = var.resource_group_name
   location            = var.location
 }
 
